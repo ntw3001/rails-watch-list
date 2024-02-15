b70b53a78971853ff678c61b2177a66cae1476a8
Nick Westbrook <ntw3001@gmail.com>
2024-02-15 16:59:27 +0900

    what a great site

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,27 @@
+class BookmarksController < ApplicationController
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
+  end
+
+  def destroy
+      @bookmark = Bookmark.find(params[:id])
+      @bookmark.destroy
+      redirect_to bookmark_path(@bookmark), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Movie.create(title: Faker::Name.name_with_middle, overview: Faker::TvShows::TheFreshPrinceOfBelAir.quote, poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: Faker::ElectricalComponents.passive, overview: Faker::String.random, poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: (Faker::Travel::TrainStation.name(region: 'united_kingdom', type: 'metro')) +" the "+ (Faker::Creature::Bird.emotional_adjective) + " " + (Faker::Creature::Bird.common_name), overview: Faker::TvShows::Seinfeld.quote, poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: Faker::Book.genre, overview: Faker::TvShows::TheITCrowd.quote, poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+puts "What a terrible collection of films"
