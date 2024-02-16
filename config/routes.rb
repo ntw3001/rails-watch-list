50df90cf3b75b4e5f28b0ab5eb908c6cf1901344
Nick Westbrook <ntw3001@gmail.com>
2024-02-16 11:53:38 +0900

    y

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  root to: "lists#index"
+
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
